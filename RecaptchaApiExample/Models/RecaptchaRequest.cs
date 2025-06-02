d5a67b4a5f3badce874de355d4db2dfee7b6ed5a
EmreCanTERKAN <emrecanterkan06@gmail.com>
2025-06-03 01:09:24 +0300

    Add project files.

---
+++ RecaptchaApiExample/Models/RecaptchaRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace RecaptchaApiExample.Models;
+
+public class RecaptchaRequest
+{
+    public string Token { get; set; } = string.Empty;
+    public string Action { get; set; } = string.Empty;
+}
---
+++ RecaptchaApiExample/Models/RecaptchaResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace RecaptchaApiExample.Models;
+
+public class RecaptchaResponse
+{
+    public bool Success { get; set; }
+    public DateTime ChallengeTs { get; set; }
+    public string Hostname { get; set; } = string.Empty;
+    public string[] ErrorCodes { get; set; } = Array.Empty<string>();
+}
